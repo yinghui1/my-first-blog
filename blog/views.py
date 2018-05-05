a76e62df5be0c047aa509a27e8778da0849aa890
susan <yysyh2010@gmail.com>
2018-05-05 14:57:04 -0700

    more templates, etc.

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 # Create your views here.
 
-def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+Post.objects.get(pk=pk)
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
